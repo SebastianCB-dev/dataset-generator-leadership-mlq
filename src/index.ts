fb8a32dcfa97f1dfba2da7b05c17002332443955
SebastianCB <sebastian.carrillob01@gmail.com>
2024-05-26 14:21:33 -0500

    Feat: add strategy by class

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,26 @@ import { generateCsvStr, createCsvFile } from './helpers'
 import { generateRow } from './helpers/generateNumbers'
 import colors from 'colors'
 
-const totalRowsToGenerate = 50
+const TOTAL_ROWS_TO_GENERATE = 6
+const SHOULD_CLASSES_BE_EQUAL = true
 const rows: Array<Array<Record<string, number>>> = []
 
-for (let i = 0; i < totalRowsToGenerate; i++) {
-  const row = generateRow()
-  rows.push(row)
+// Strategy to generate equal classes
+if (SHOULD_CLASSES_BE_EQUAL) {
+  let classToGenerate: number = 0
+  for (let i = 0; i < TOTAL_ROWS_TO_GENERATE; i++) {
+    let row = []
+    do {
+      row = generateRow()
+    } while (row[row.length - 1].Class !== classToGenerate)
+    classToGenerate === 2 ? classToGenerate = 0 : classToGenerate++
+    rows.push(row)
+  }
+} else {
+  for (let i = 0; i < TOTAL_ROWS_TO_GENERATE; i++) {
+    const row = generateRow()
+    rows.push(row)
+  }
 }
 
 try {
